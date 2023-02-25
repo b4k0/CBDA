3e8139455faf2bb5d3c42005ece6df107d1e4b84
Mpak1996 <giannismpak@outlook.com>
2023-02-25 03:00:13 +0200

    Update Algorithms page with textboxes for ml model & vectorizer

---
+++ pages/2_📊_Algorithms.py
@@ -0,0 +1,49 @@
+import streamlit as st
+from PIL import Image
+import pickle
+import string
+import re
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer 
+import time
+
+hide_menu = """
+<style>
+#MainMenu{
+    visibility:hidden;
+}
+
+footer{
+    visibility:hidden;
+}
+</style>
+"""
+
+showWarningOnDirectExecution = False
+ps = PorterStemmer()
+image = Image.open('logo.png')
+
+
+st.set_page_config(page_title = "Algorithms", page_icon = image)
+
+st.markdown(hide_menu, unsafe_allow_html=True)
+
+ 
+st.sidebar.image(image , use_column_width=True, output_format='auto')
+
+
+st.sidebar.markdown("---")
+
+
+st.sidebar.markdown("<br> <br> <br> <br> <br> <br> <br> <br> <h1 style='text-align: center; font-size: 18px; color: #0080FF;'>Ioannis Bakomichalis © 2023</h1>", unsafe_allow_html=True)
+
+
+
+
+st.title("Algorithms📊")
+
+all_Vectorizers = ["TF-IDF", "Count Vectorizer"]
+vect_choice = st.selectbox("Choose Vectorizer", all_Vectorizers)
+all_ML_models = ["Logistic Regression", "Decision Tree", "Random Forest", "XGBoost", "Naive Bayes", "Support Vector Machine", "Bagging Decision Tree", "Boosting Decision Tree"]
+model_choice = st.selectbox("Choose Machine Learning Algorithm", all_ML_models)
